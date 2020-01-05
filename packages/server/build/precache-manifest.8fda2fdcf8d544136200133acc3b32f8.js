6de762270ffbc24006fbca8255962435ad80bf5b
quahjieren@gmail.com <quahjieren@gmail.com>
2020-01-06 00:06:31 +0800

    Added proxy to client, configure server for deployment

---
+++ packages/server/build/precache-manifest.8fda2fdcf8d544136200133acc3b32f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fdaefc0697b2a97e51a38bbae709be8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9bef7f9b80c4960ee5be",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e7c80db38b1cab8734b7",
+    "url": "/static/js/2.31636bbd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.31636bbd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9bef7f9b80c4960ee5be",
+    "url": "/static/js/main.89658326.chunk.js"
+  },
+  {
+    "revision": "5536b1ffa5673e4eb964",
+    "url": "/static/js/runtime-main.d07f956b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
